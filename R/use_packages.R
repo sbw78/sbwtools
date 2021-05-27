26b87447314eb0164fed204a2fa3e289910be87d
Bryan West <sbw78@cornell.edu>
2021-05-27 15:06:40 -0400

    Update package version

--- R/use_packages.R
+++ R/use_packages.R
@@ -5,10 +5,6 @@
 #' @export
 use_packages <- function (open = interactive()) {
   usethis::use_directory("R")
-  scripts <- c(file.path("packages.R"))
-  for (script in scripts) {
-    usethis::use_template(file.path("usepackages", script),
-                          save_as = file.path("R", script), package = "sbwtools", open = open)
-  }
+  usethis::use_template("packages.R", package = "sbwtools")
   invisible()
 }
