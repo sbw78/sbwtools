2a1eb806dbe6473aa35c15ed6a3c8bc14f1184ad
Bryan West <sbw78@cornell.edu>
2021-05-27 15:03:08 -0400

    Removed R subdirectory from templates folder

--- R/use_packages.R
+++ R/use_packages.R
@@ -5,10 +5,10 @@
 #' @export
 use_packages <- function (open = interactive()) {
   usethis::use_directory("R")
-  scripts <- c(file.path("R", "packages.R"))
+  scripts <- c(file.path("packages.R"))
   for (script in scripts) {
     usethis::use_template(file.path("usepackages", script),
-                          save_as = script, package = "sbwtools", open = open)
+                          save_as = file.path("R", script), package = "sbwtools", open = open)
   }
   invisible()
 }
