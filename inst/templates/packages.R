7e7032dc8b8dd429a577c75bebb8c7b27f75dfee
Bryan West <sbw78@cornell.edu>
2021-08-13 17:12:45 -0400

    Updated packages.R template file

--- inst/templates/packages.R
+++ inst/templates/packages.R
@@ -1,85 +1,134 @@
-pkgs_install <- c(
-  # "conflicted",
-  # "dotenv",
-  # "targets",
-  # "tarchetypes",
-  # "performance",
-  # "knitr",
-  # "kableExtra",
-  # "ragg",
-  # "labelled",
-  # "lmtest",
-  # "lmerTest",
-  # "modelsummary",
-  # "psych",
-  # "GPArotation",
-  # "RColorBrewer",
-  # "here",
-  # "haven",
-  # "ggplot2",
-  # "ggrepel",
-  # "ggraph",
-  # "tidygraph",
-  # "ggstatsplot",
-  # "cowplot",
-  # "lemon",
-  # "dplyr",
-  # "tidyr",
-  # "stringr",
-  # "purrr",
-  # "forcats",
-  # "tibble",
-  # "ggtext",
-  # "gt",
-  # "jacob-long/interactions",
-  # "jacob-long/jtools",
-  # "strengejacke/strengejacke",
-  # "strengejacke/sjPlot",
-  # "modelsummary",
-  # "sandwich",
-  # "lavaan",
-  # "tinytex",
-  # "rmdformats",
-  # "broom",
-  # "broom.mixed",
-  # "tictoc",
-  # "rmarkdown",
-  # "janitor",
-  # "emmeans",
-  # "effectsize",
-  # "easystats/report",
-  # "dr-JT/semoutput",
-  # "sbw78/fancyCorr",
-  # "cocor",
-  # "sbw78/sbwtools",
-  # "crsh/papaja",
-  # "cjvanlissa/tidySEM",
-  # "jasonmoy28/psycModel",
-  # "gridExtra",
-  # "qqplotr",
-  # "see",
-  # "nFactors"
-)
+# Packages to install ----
 
-# invisible(
-lapply(pkgs_install, function(pkg) {
-  y <- unlist(
-    lapply(pkg,
-           function(x) {
-             ifelse(grepl("/", x),
-                    unlist(strsplit(x, "/"))[2],
-                    x)
-           }
+{
+  unlist(
+    list(
+      ## Utilities ----
+      c(
+        # "conflicted",
+        # "tictoc",
+        # "here",
+        # "ragg",
+        # "sbw78/sbwtools"
+      ),
+      ## Data manipulation ----
+      c(
+        # "tidyverse",
+        # "labelled",
+        # "haven",
+        # "lubridate",
+        # "janitor"
+      ),
+      ## Correlations ----
+      c(
+        # "sbw78/fancyCorr",
+        # "cocor"
+      ),
+      ## Modeling ----
+      c(
+        # "lmtest",
+        # "lmerTest"
+      ),
+      ### SEM / Factor Analysis
+      c(
+        # "lavaan",
+        # "cjvanlissa/tidySEM",
+        # "nFactors",
+      ),
+
+      ### Model Analysis
+      c(
+        # "emmeans",
+        # "performance",
+        # "modelsummary",
+        # "jacob-long/interactions",
+        # "jacob-long/jtools",
+        # "strengejacke/strengejacke",
+        # "modelsummary",
+        # "effectsize",
+        # "easystats/report",
+        # "sandwich"
+      ),
+
+      ### Summarizing model data / tidying
+      c(
+        # "broom",
+        # "broom.mixed",
+        # "gt"
+      ),
+
+      ## Project workflow (targets/drake) ----
+      c(
+        # "dotenv",
+        # "targets",
+        # "tarchetypes",
+      ),
+
+      ## RMarkdown / document production ----
+      c(
+        # "rmarkdown",
+        # "knitr",
+        # "kableExtra",
+        # "rmdformats"
+        # "tinytex",
+        # "crsh/papaja"
+      ),
+
+      ## Psychology-related ----
+      c(
+        # "psych",
+        # "GPArotation",
+        # "jasonmoy28/psycModel"
+      ),
+      ## Data visualization ----
+      c(
+        # "ggplot2",
+        # "RColorBrewer",
+        # "ggrepel",
+        # "ggraph",
+        # "tidygraph",
+        # "ggstatsplot",
+        # "cowplot",
+        # "lemon",
+        # "ggtext",
+        # "strengejacke/sjPlot",
+        # "dr-JT/semoutput",
+        # "gridExtra",
+        # "qqplotr",
+        # "see"
+      )
+      ## End of package list ----
     )
   )
-  if (!require(y, character.only = TRUE)) renv::install(pkg)
-})
+} -> pkgs_install
+
+# Install and load packages ----
+# invisible(
+lapply(
+  pkgs_install,
+  FUN = function(pkg) {
+    y <- unlist(
+      lapply(pkg,
+             function(x) {
+               ifelse(grepl("/", x),
+                      unlist(strsplit(x, "/"))[2],
+                      x)
+             }
+      )
+    )
+    if (!require(y, character.only = TRUE)) {
+      renv::install(pkg)
+    }
+    library(y, character.only = TRUE)
+  }
+)
 # )
 
 rm(pkgs_install)
 
-# conflicted::conflict_prefer("filter", "dplyr", quiet = TRUE)
+#
+conflicted::conflict_prefer("filter", "dplyr", quiet = TRUE)
 # conflicted::conflict_prefer("report", "report", quiet = TRUE)
-# conflicted::conflict_prefer("to_factor", "labelled", quiet = TRUE)
-# conflicted::conflict_prefer("as_factor", "forcats", quiet = TRUE)
+conflicted::conflict_prefer("to_factor", "labelled", quiet = TRUE)
+conflicted::conflict_prefer("as_factor", "forcats", quiet = TRUE)
 # conflicted::conflict_prefer("google_font", "xaringanthemer", quiet = TRUE)
